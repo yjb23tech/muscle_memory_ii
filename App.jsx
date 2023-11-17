3d03b8cd00b7f043dddd27a71f023df98fd433e5
yjb23tech <yjb23tech@gmail.com>
2023-11-17 19:10:09 +0000

    Rozay

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 
 import { createRoot } from "react-dom/client"
+import Header from "./components/Header.jsx"
 
 const App = () => {
     return (
         <div>
+            <Header />
             <h2>Sanity Check Extended</h2>
         </div>
     )
---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../images/react-logo.png"
+
+function Header() {
+    return (
+        <div>
+            <nav>
+                <img src={logo} width="60px" />
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
